2bbe7b02d5911cd005761a5b71347ada1a7685f0
rafael.moura1 <rafael.moura1@estudante.ufla.br>
2021-01-06 19:09:05 -0300

    codigo fonte

---
+++ REO 5 - MATRIZES E VETORES/Vetores/vetor_registro_ordenacao_busca_bolao_copa.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+
+struct time {
+    int identificador;
+    string nome;
+    int gols_marcados;
+};
+
+
+void copiarDeTimeParaOutro( time& original, time& copia ) {
+    copia.identificador = original.identificador;
+    copia.nome = original.nome;
+    copia.gols_marcados = original.gols_marcados;
+}
+
+
+
+int procuraTime( time meuVetorDeTimes[ ], int identificador, int tamanho ) {
+    int timeInexistente = -1;
+    for ( int posicao = 0; posicao < tamanho; posicao++ ) {
+        if ( meuVetorDeTimes[posicao].identificador == identificador ) {
+            return posicao;
+        }
+    }
+    return timeInexistente;
+}
+
+void intercala( time meuVetorDeTimes[ ], int inicio, int meio, int fim ) {
+    int i = inicio, j = meio + 1;
+    int tamanho = fim - inicio + 1;
+    time aux[tamanho]; // vetor auxiliar
+    for ( int k = 0; k < tamanho; k++ ) {
+        if ( ( i <= meio ) and ( j <= fim ) ) {
+            if ( meuVetorDeTimes[i].identificador <= meuVetorDeTimes[j].identificador ) {
+                copiarDeTimeParaOutro( meuVetorDeTimes[i], aux[k] ); // copia trecho1 em aux[]
+                i++; // avança em trecho1
+            }
+            else { //
+                copiarDeTimeParaOutro( meuVetorDeTimes[j], aux[k] ); // copia trecho2 em aux[]
+                j++; // avanca em trecho2
+            }
+
+        }
+        else if ( i > meio ) { // terminou o trecho1
+            copiarDeTimeParaOutro( meuVetorDeTimes[j], aux[k] );
+            j++;
+        }
+        else { // terminou o trecho2
+            copiarDeTimeParaOutro( meuVetorDeTimes[i], aux[k] );
+            i++;
+        }
+    }
+    // terminando: copiar de aux[] em a[inicio:fim]
+    for ( int k = 0; k < tamanho; k++ ) {
+        copiarDeTimeParaOutro( aux[k], meuVetorDeTimes[inicio + k] );
+    }
+}
+
+void mergesort( time meuVetorDeTimes[ ], int inicio, int fim ) {
+    int meio;
+    if ( inicio < fim ) {
+        meio = ( inicio + fim ) / 2;
+        mergesort( meuVetorDeTimes, inicio, meio );
+        mergesort( meuVetorDeTimes, meio + 1, fim );
+        intercala( meuVetorDeTimes, inicio, meio, fim );
+    }
+}
+
+
+
+int main() {
+    int tamanho;
+    cin >> tamanho;
+    time meuVetorDeTimes[tamanho];
+    for ( int i = 0; i < tamanho; i++ ) {
+        cin >> meuVetorDeTimes[i].identificador >> meuVetorDeTimes[i].nome >> meuVetorDeTimes[i].gols_marcados;
+    }
+    int identificador;
+    cin >> identificador;
+
+    mergesort( meuVetorDeTimes, 0, tamanho - 1 );
+    int posicao = procuraTime( meuVetorDeTimes, identificador, tamanho );
+    cout << posicao << endl;
+    if ( posicao >= 0 ) {
+        cout << meuVetorDeTimes[posicao].nome << " " << meuVetorDeTimes[posicao].gols_marcados << endl;
+    }
+
+    return 0;
+}
