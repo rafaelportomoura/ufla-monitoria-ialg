14b13d64e92feb38caf0664e234fffe92109b61b
rafael.moura1 <rafael.moura1@estudante.ufla.br>
2021-01-07 10:12:41 -0300

    nomes mais declarativos

--- REO 5 - MATRIZES E VETORES/Matrizes/matriz_itinerario_rodoviario.cpp
+++ REO 5 - MATRIZES E VETORES/Matrizes/matriz_itinerario_rodoviario.cpp
@@ -4,34 +4,34 @@ using namespace std;
 int main() {
     string cidades[5];
     int distancia[5][5];
-    float combustivel;
-    for (int i = 0; i < 5; i++) {
+    float consumo;
+    for ( int i = 0; i < 5; i++ ) {
         cin >> cidades[i];
     }
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            if (i == j) {
-                distancia[i][j] = 0;
+    for ( int linha = 0; linha < 5; linha++ ) {
+        for ( int coluna = 0; coluna < 5; coluna++ ) {
+            if ( linha == coluna ) {
+                distancia[linha][coluna] = 0;
             }
             else {
-                cin >> distancia[i][j];
+                cin >> distancia[linha][coluna];
             }
         }
     }
-    cin >> combustivel;
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            if (distancia[i][j] < 250 && distancia[i][j] != 0) {
-                cout << cidades[i] << " " << cidades[j] << endl;
+    cin >> consumo;
+    for ( int linha = 0; linha < 5; linha++ ) {
+        for ( int coluna = 0; coluna < 5; coluna++ ) {
+            if ( distancia[linha][coluna] < 250 && distancia[linha][coluna] != 0 ) {
+                cout << cidades[linha] << " " << cidades[coluna] << endl;
             }
         }
     }
-    float consumo = 0;
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            if (distancia[i][j] != 0) {
-                consumo = distancia[i][j] / combustivel;
-                cout << cidades[i] << " " << cidades[j] << " " << consumo << endl;
+    float combustivel = 0;
+    for ( int linha = 0; linha < 5; linha++ ) {
+        for ( int coluna = 0; coluna < 5; coluna++ ) {
+            if ( distancia[linha][coluna] != 0 ) {
+                combustivel = distancia[linha][coluna] / consumo;
+                cout << cidades[linha] << " " << cidades[coluna] << " " << combustivel << endl;
             }
         }
     }
